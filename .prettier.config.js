40dba4941bcdb339a6fe3b3a618a5a1e12d488e3
marcelobarth <marcelinhobarth@hotmail.com>
2020-12-18 17:11:37 -0300

    Nivel 2 finalizado

---
+++ .prettier.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  // aspas simples
+  singleQuote: true,
+  // No final dos objetivos ira por a virgula
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '06f04a4fde638360fc6fb5594a033d52',
+    expiresIn: '1d',
+  }
+}
